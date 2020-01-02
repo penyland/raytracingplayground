65d5c4e46de468adcb32cd067e85de04515773d9
Peter Nylander <peter.nylander@gmail.com>
2020-01-02 11:24:20 +0100

    Inital repo setup and exercise 1

---
+++ src/RaytracingPlayground/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Peter Nylander. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RaytracingPlayground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RayTracer rayTracer;
+        private Microsoft.Graphics.Canvas.CanvasRenderTarget renderTarget;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnRender_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.rayTracer == null)
+            {
+                this.rayTracer = new RayTracer((float)this.canvas.ActualWidth, (float)this.canvas.ActualHeight, (int)this.canvas.Dpi);
+            }
+
+            this.renderTarget = await this.rayTracer.RenderAsync();
+
+            this.canvas.Invalidate();
+        }
+
+        private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+            if (this.renderTarget == null)
+            {
+                args.DrawingSession.Clear(Colors.CornflowerBlue);
+                args.DrawingSession.DrawText("Hello, World!", 100, 100, Colors.Black);
+                args.DrawingSession.DrawCircle(125, 125, 100, Colors.Green);
+                args.DrawingSession.DrawLine(0, 0, 50, 200, Colors.Red);
+            }
+            else
+            {
+                args.DrawingSession.DrawImage(this.renderTarget);
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.canvas.RemoveFromVisualTree();
+            this.canvas = null;
+        }
+    }
+}
---
+++ src/RaytracingPlayground/RayTracer.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Peter Nylander. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+using Windows.UI;
+
+namespace RaytracingPlayground
+{
+    public class RayTracer
+    {
+        private CanvasRenderTarget renderTarget;
+
+        public RayTracer(float width, float height, int dpi)
+        {
+            CanvasDevice canvasDevice = CanvasDevice.GetSharedDevice();
+            this.renderTarget = new CanvasRenderTarget(
+                canvasDevice,
+                width,
+                height,
+                dpi,
+                Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                CanvasAlphaMode.Premultiplied);
+        }
+
+        public CanvasRenderTarget RenderTarget { get => this.renderTarget; }
+
+        public async Task<CanvasRenderTarget> RenderAsync()
+        {
+            int width = (int)this.RenderTarget.SizeInPixels.Width;
+            int height = (int)this.RenderTarget.SizeInPixels.Height;
+
+            using (var ds = this.renderTarget.CreateDrawingSession())
+            {
+                ds.Clear(Color.FromArgb(0xff, 0xee, 0xaa, 0x44));
+            }
+
+            byte[] buffer = this.renderTarget.GetPixelBytes();
+
+            for (int j = 0; j < height; ++j)
+            {
+                var total_row_len = j * width;
+                for (uint i = 0; i < width; ++i)
+                {
+                    var index = (total_row_len + i) * 4;
+
+                    float r = (float)i / width;
+                    float g = (float)j / height;
+                    float b = (float)0.2f;
+
+                    buffer[index + 0] = (byte)(b * 0xFF);
+                    buffer[index + 1] = (byte)(g * 0xFF);
+                    buffer[index + 2] = (byte)(r * 0xFF);
+                    buffer[index + 3] = 1;
+                }
+            }
+
+            this.renderTarget.SetPixelBytes(buffer);
+
+            return this.renderTarget;
+        }
+    }
+}
